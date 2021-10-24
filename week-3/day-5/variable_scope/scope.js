cd30a5d2613e14895ab4f35a09cc99e4604301c5
J-eld <70251176+J-eld@users.noreply.github.com>
2021-10-24 13:47:00 +1300

    added content for day-5

---
+++ week-3/day-5/variable_scope/scope.js
@@ -0,0 +1,17 @@
+let x = 'pen';
+
+function someFunc() {
+    let myVar = 'paper';
+    console.log(myVar)
+    if (true) {
+        let someOtherVar = 'variable'
+    }
+    console.log(someOtherVar)
+}
+
+someFunc()
+
+function someOtherFunc() {
+    console.log(myVar)
+}
+
