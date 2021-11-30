d6f6d2be0c1bed1d4ae0f8bd8861643dfe132484
J-eld <70251176+J-eld@users.noreply.github.com>
2021-11-30 22:23:09 +1300

    added mui-layout examples

---
+++ week-9/day-2/mui-examples/src/components/MuiContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '@mui/material/Container';
+
+export default function MuiContainer() {
+  return (
+    <div>
+      <Container maxWidth="lg" >
+        <div style={{backgroundColor: 'blue', height: '400px'}}>
+
+        </div>
+      </Container>
+    </div>
+  )
+}
