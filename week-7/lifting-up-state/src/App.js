6aeb2b9d52629c654eee5c38cc3fc305bd8097ab
Sebin Benjamin <sebinbenjamin@gmail.com>
2022-06-17 09:40:51 +1200

    fix: update lifting state example code

--- week-7/lifting-up-state/src/App.js
+++ week-7/lifting-up-state/src/App.js
@@ -8,12 +8,9 @@ function App() {
   const handleInputChange = (e) => setVal(e.target.value);
 
   return (
-    <div style={{ border: '2px solid black', margin: '5px' }}>
+    <div style={{ border: '2px solid red', margin: '5px', padding: '2px' }}>
       {/*orange component  */}
-      <InputComponent
-        val={val}
-        handleInputChange={handleInputChange}
-      ></InputComponent>
+      <InputComponent val={val} handleInputChange={handleInputChange}></InputComponent>
       <br />
       {/*green component */}
       <ShowInput val={val}></ShowInput>
--- week-7/lifting-up-state/src/components/InputComponent.js
+++ week-7/lifting-up-state/src/components/InputComponent.js
@@ -1,20 +1,8 @@
-import { useState } from 'react';
-
 export default function InputComponent({ val, handleInputChange }) {
-  const [inputToShow, setInputToShow] = useState('');
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    setInputToShow(val);
-  };
-
   return (
     <div style={{ border: '2px dashed orange' }}>
-      <form onSubmit={handleFormSubmit}>
-        <input type="text" value={val} onChange={handleInputChange} />
-        <button type="submit"> Submit </button>
-      </form>
-      <h1>{inputToShow}</h1>
+      This is the InputComponent <br></br>
+      <input type="text" value={val} onChange={handleInputChange} />
     </div>
   );
 }
