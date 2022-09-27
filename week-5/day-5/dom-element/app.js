90d40a45aa6997c5ff3eca2be33e991b7b1fdcc5
Sebin Benjamin <sebinbenjamin@gmail.com>
2022-09-27 14:22:04 +1300

    feat: update week 6 content

---
+++ week-5/day-5/dom-element/app.js
@@ -0,0 +1,13 @@
+// Creating a new html div element
+const divElement = document.createElement('div');
+divElement.innerText = 'Hello World';
+
+// adding the div created to the body of the HTML doc
+document.body.appendChild(divElement);
+
+const imageElement = document.createElement('img');
+imageElement.src = 'https://media.istockphoto.com/photos/funny-winking-kitten-picture-id1267021092?k=20&m=1267021092&s=612x612&w=0&h=yzwxZXklHn5NwDTgKmbq2Ojtg3pga6j8K3oT7lLneAY=';
+document.body.appendChild(imageElement);
+
+
+
